8162f68b2f283247f1b5b0e0f8ffb14b2ec56da6
JamalMydeen <jamessies123@gmail.com>
2023-05-29 22:24:56 +0530

    outer page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Outer from './pages/outer';
+import { Routes , Route } from 'react-router-dom';
 
 function App() {
   return (
-   <h1>hello world!</h1>
+     <Routes>
+      <Route path='/' element={<Outer/>}/>
+     </Routes>
   );
 }
 
--- client/src/pages/outer.js
+++ client/src/pages/outer.js
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import Background from "../images/background2.jpg";
+import "../css/outer.css";
 const Outer = () => {
   return (
-    
+    <div className="outer">
+    <div className="image">
+      <img src={Background} alt="bgpic" />
+    </div>
+    <div className="texts">
+      <h2>All Your <span className='change'>Notes</span> In <br />One Place</h2>
+      <p>Take notes & manage yout notes <br /> with eaise on sticky</p>
+    </div>
+    <div className="getstarted">
+      <button className='btn'>Create Account</button>
+      <button className='btn'>Log in</button>
+     </div>
+    </div>
   )
 }
 
