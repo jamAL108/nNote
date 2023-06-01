31957a73c6c173d6525ee02fb55d2ab4aa868301
JamalMydeen <jamessies123@gmail.com>
2023-06-01 15:49:37 +0530

    UI

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -70,7 +70,7 @@ const Home = () => {
               let tp = temp6.current;
               tp.style.zIndex="0";
               var scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
-              scrollTop+=0.8;
+              scrollTop+=1;
               tp.style.color="transparent";
               tp.style.top=`${scrollTop}px`;
           }}/>
@@ -99,7 +99,7 @@ const Home = () => {
               temp1.style.top=`${scrollTop}px`;
               let tp = temp6.current;
               tp.style.zIndex="7";
-              scrollTop+=0.8;
+              scrollTop+=1;
               tp.style.color="black";
               tp.style.top=`${scrollTop}px`;
           }}/>
