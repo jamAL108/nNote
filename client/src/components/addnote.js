98128b735c448dc5f40be684fac0c1e37d01c6e1
JamalMydeen <jamessies123@gmail.com>
2023-06-02 17:24:49 +0530

    UI

--- client/src/components/addnote.js
+++ client/src/components/addnote.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PushPinOutlinedIcon from '@mui/icons-material/PushPinOutlined';
 import ArchiveOutlinedIcon from '@mui/icons-material/ArchiveOutlined';
 import ArrowBackOutlinedIcon from '@mui/icons-material/ArrowBackOutlined';
+import PushPinIcon from '@mui/icons-material/PushPin';
+import ArchiveIcon from '@mui/icons-material/Archive';
 import { useNavigate } from 'react-router-dom';
 import './addnote.css';
 const Addnote = () => {
+    const [pin,setpin] = useState(false);
+    const [archive ,setarchive] = useState(false);
     const navigate = useNavigate();
   return (
     <div className="addnote">
@@ -15,16 +19,24 @@ const Addnote = () => {
          }} />
          </div>
          <div className="right">
-            <PushPinOutlinedIcon/>
-            <ArchiveOutlinedIcon/>
+         <div className="grid" onClick={(e)=>{
+            setpin(!pin);
+           }} >
+          {pin ? <PushPinIcon className='bttn'/> : <PushPinOutlinedIcon className='bttn' />}
+       </div>
+       <div className="grid" onClick={(e)=>{
+            setarchive(!archive);
+           }} >
+          {archive ? <ArchiveIcon className='bttn'/> : <ArchiveOutlinedIcon className='bttn'/>}
+       </div>
          </div>
       </header>
       <div className="content">
        <input className='input1' type="text" placeholder='Title'/>
-       <textarea placeholder='Note'  cols="30" rows="30"></textarea>
+       <textarea placeholder='Note' type="text" className='input2' cols="30" rows="30"></textarea>
       </div>
       <div className="down">
-
+         
       </div>
 
     </div>
